3875e0023d43b8f2f9d3f110146e41944c07b536
monomonosu <pojinyan@yahoo.co.jp>
2025-01-13 22:39:56 +0900

    StoryBookとページ上でradix-uiを使用できるように設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
-				{children}
+				<Theme>{children}</Theme>
 			</body>
 		</html>
 	);
