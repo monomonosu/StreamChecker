6ea7082bc8aa273bd86f7e601f7ac5a1006a1406
monomonosu <pojinyan@yahoo.co.jp>
2025-02-11 23:03:59 +0900

    Headerコンポーネント作成

---
+++ src/app/_components/server/Header/index.tsx
@@ -0,0 +1,30 @@
+import { HamburgerMenuIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import Link from "next/link";
+
+import style from "@/app/_components/server/Header/style/style.module.scss";
+
+export const Header = () => {
+	return (
+		<header className={style.header}>
+			<div className={style.header_inner}>
+				<Link className={style.link} href="/">
+					<Image src="/test-logo.svg" height={24} alt="" width={100} />
+				</Link>
+				<Link className={style.link} href="/">
+					ホーム
+				</Link>
+				<Link className={style.link} href="/new">
+					新着
+				</Link>
+				<Link className={style.link} href="/pick-up">
+					人気
+				</Link>
+			</div>
+			<div className={style.header_inner}>
+				<MagnifyingGlassIcon width={24} height={24} />
+				<HamburgerMenuIcon width={24} height={24} />
+			</div>
+		</header>
+	);
+};
