00b4ebbab4a320b191402839fa8b521a2626ab37
monomonosu <pojinyan@yahoo.co.jp>
2025-03-13 00:24:47 +0900

    ジャンル周りをジャンル用のコンポーネントに切り替え

--- src/app/(static)/page.tsx
+++ src/app/(static)/page.tsx
@@ -6,6 +6,7 @@ import { getNewReleases } from "@/app/_fetchers/getNewReleases";
 import { getPopularityAlbums } from "@/app/_fetchers/getPopularityAlbums";
 
 import { genres } from "@/app/(static)/_utils/_constants";
+import { Genre } from "@/app/_components/client/Genre/Genre";
 
 export default async function Home() {
 	// 最新のおすすめ
@@ -58,11 +59,12 @@ export default async function Home() {
 				<h1>ジャンル</h1>
 				<JacketSlider>
 					{genres.map((genre) => (
-						<Jacket
+						<Genre
 							key={genre.id}
 							href="/"
 							priority
 							src={genre.src}
+							genreName={genre.name}
 							width={200}
 							height={200}
 							alt="ジャンル画像"
