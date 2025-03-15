8b7c6e38c1c4cb896e7d13321b862eb3ea4233b0
monomonosu <pojinyan@yahoo.co.jp>
2025-03-16 00:18:38 +0900

    stories作成

---
+++ src/stories/layouts/Loading.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Loading } from "@/app/_components/layouts/Loading";
+
+const meta = {
+	title: "Layouts/Loading",
+	component: Loading,
+	parameters: {
+		layout: "fullscreen", // or `padded` by default
+	},
+	tags: ["autodocs"],
+} satisfies Meta<typeof Loading>;
+
+export default meta;
+type Story = StoryObj<typeof Loading>;
+
+export const Default: Story = {
+	render: () => (
+		<div style={{ width: "100%" }}>
+			<Loading />
+		</div>
+	),
+};
