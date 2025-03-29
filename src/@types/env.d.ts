ba62b8eab97eacd916508ad9bfa09cdb83abfbac
monomonosu <pojinyan@yahoo.co.jp>
2025-03-29 23:26:35 +0900

    検索結果の再生数トップの動画を取得するAPIRoute作成

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
 	interface ProcessEnv {
+		readonly YOUTUBE_API_KEY: string;
+		readonly YOUTUBE_API_BASE_URL: string;
 		readonly SPOTIFY_CLIENT_ID: string;
 		readonly SPOTIFY_CLIENT_SECRET: string;
 		readonly SPOTIFY_API_BASE_URL: string;
