c81b46dd27c2662a8c3fcab2cf747aa26bd58001
monomonosu <pojinyan@yahoo.co.jp>
2025-03-29 19:52:33 +0900

    人気曲用のリストコンポーネント作成

---
+++ src/app/_components/client/TrackTable/TrackTable.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Image from "next/image";
+
+import styles from "@/app/_components/client/TrackTable/track-list.module.scss";
+import helper from "@/app/_styles/helper.module.scss";
+
+type Track = {
+	id: string;
+	index: number;
+	image: string;
+	title: string;
+	album: string;
+	duration: number;
+};
+
+type Props = {
+	tracks: Track[];
+};
+
+export const TrackList = ({ tracks }: Props) => {
+	const chunkedTracks = Array.from(
+		{ length: Math.ceil(tracks.length / 3) },
+		(_, i) => tracks.slice(i * 3, i * 3 + 3),
+	);
+
+	console.log(chunkedTracks);
+
+	return (
+		<div className={styles.scrollWrapper}>
+			<div className={styles.gridContainer}>
+				{chunkedTracks.map((group, columnIndex) => (
+					<div className={styles.column} key={String(columnIndex)}>
+						<ul className={styles.list}>
+							{group.map((track) => (
+								<li key={track.id} className={styles.row}>
+									<span className={helper.textEllipsis}>{track.index}</span>
+									<span>
+										<Image
+											className={styles.image}
+											src={track.image}
+											alt="トラック画像"
+											width={40}
+											height={40}
+										/>
+									</span>
+									<span className={helper.textEllipsis}>{track.title}</span>
+									<span className={helper.textEllipsis}>{track.album}</span>
+									<span className={helper.textEllipsis}>{track.duration}</span>
+								</li>
+							))}
+						</ul>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
