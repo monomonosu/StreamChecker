96bdc494833fdc58f036688fc49afd46ecd6b8d3
monomonosu <pojinyan@yahoo.co.jp>
2025-04-05 23:35:52 +0900

    FooterPlayerコンポーネント作成

---
+++ src/app/_components/layouts/FooterPlayer.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Theme } from "@radix-ui/themes";
+import { useAtomValue } from "jotai";
+
+import { isOpenFooterAtom, videoIdAtom } from "@/libs/stores/video";
+
+import style from "@/app/_components/layouts/footer-player.module.scss";
+
+type Props = {
+	height: number;
+	width: number;
+};
+
+export const FooterPlayer = ({ height = 280, width = 140 }: Props) => {
+	const isOpenFooter = useAtomValue(isOpenFooterAtom);
+	const videoId = useAtomValue(videoIdAtom);
+	const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+
+	return (
+		<>
+			{isOpenFooter && (
+				<Theme appearance="dark" className={style.footer}>
+					<iframe
+						width={width}
+						height={height}
+						src={embedUrl}
+						title="YouTube video player"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					/>
+				</Theme>
+			)}
+		</>
+	);
+};
