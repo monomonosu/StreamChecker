4b9a59ee6a62319f5b8628059deeb6f604c3b65d
monomonosu <pojinyan@yahoo.co.jp>
2025-04-08 02:39:30 +0900

    ヘッダーコンポーネントのレスポンシブ対応

--- src/app/_components/server/Header/Header.tsx
+++ src/app/_components/server/Header/Header.tsx
@@ -1,7 +1,11 @@
-import { HamburgerMenuIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
-import { Theme } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
+
+import { HamburgerMenuIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Theme } from "@radix-ui/themes";
+import { House } from "lucide-react";
+import { Bell } from "lucide-react";
+import { ChartNoAxesCombined } from "lucide-react";
 
 import style from "@/app/_components/server/Header/header.module.scss";
 
@@ -9,17 +13,33 @@ export const Header = () => {
 	return (
 		<Theme appearance="dark" className={style.header}>
 			<div className={style.header_inner}>
-				<Link className={style.link} href="/">
-					<Image src="/test-logo.svg" height={24} alt="" width={100} />
+				<Link className={style.link} style={{ height: "24px" }} href="/">
+					<Image
+						className={style.logo}
+						src="/test-logo.svg"
+						height={24}
+						alt="logo"
+						width={100}
+					/>
+					<Image
+						className={style.logoSmall}
+						src="/test-logo.svg"
+						height={24}
+						alt="logo"
+						width={24}
+					/>
 				</Link>
 				<Link className={style.link} href="/">
-					ホーム
+					<span className={style.label}>ホーム</span>
+					<House className={style.icon} size="24px" />
 				</Link>
 				<Link className={style.link} href="/new">
-					新着
+					<span className={style.label}>新着</span>
+					<Bell className={style.icon} size="24px" />
 				</Link>
 				<Link className={style.link} href="/pick-up">
-					人気
+					<span className={style.label}>人気</span>
+					<ChartNoAxesCombined className={style.icon} size="24px" />
 				</Link>
 			</div>
 			<div className={style.header_inner}>
