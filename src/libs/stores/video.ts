c73fcc536b1a53b8bd0fc08a71ebff8546e78578
monomonosu <pojinyan@yahoo.co.jp>
2025-05-03 17:13:09 +0900

    不要Atom削除

--- src/libs/stores/video.ts
+++ src/libs/stores/video.ts
@@ -1,36 +1,15 @@
 import { atom } from "jotai";
 
 /**
- * 再生中の動画ID
- */
-export const videoIdAtom = atom("");
-
-/**
  * 再生中の動画のタイトル
  */
 export const videoTitleAtom = atom("");
-
-/**
- * 再生中の動画のチャンネル名
- */
-export const videoChannelAtom = atom("");
 
 /**
  * Footerの表示・非表示
  * @type {boolean}
  */
 export const isOpenFooterAtom = atom(false);
-
-/**
- * 再生ステータス
- * @type {"playing" | "paused" | "ended" | "none"}
- * @constant {string}
- * @default "none"
- * @description 再生中の動画の状態を管理する
- */
-export const playStateAtom = atom<"playing" | "paused" | "ended" | "none">(
-	"none",
-);
 
 /**
  * トラックテーブルで選択されたトラックのID
