4b64516ec3627f8ed394467094e925ca4b4e762b
monomonosu <pojinyan@yahoo.co.jp>
2025-05-17 22:20:24 +0900

    Spotifyアルバム情報取得用のfetcherと型を作成

--- src/app/_fetchers/types.ts
+++ src/app/_fetchers/types.ts
@@ -95,3 +95,101 @@ export type SpotifyArtistAlbumsResponse = {
 export type SpotifyArtistTopTracksResponse = {
 	tracks: SpotifyTrackItem[];
 };
+
+/**
+ * アルバムの情報
+ * @see /albums/[id]
+ */
+export type SpotifyAlbumResponse = {
+	album_type: "album" | "single" | "compilation";
+	total_tracks: number;
+	available_markets: string[];
+	external_urls: {
+		spotify: string;
+	};
+	href: string;
+	id: string;
+	images: Array<{
+		url: string;
+		height: number | null;
+		width: number | null;
+	}>;
+	name: string;
+	release_date: string;
+	release_date_precision: "year" | "month" | "day";
+	restrictions?: {
+		reason: "market" | "product" | "explicit";
+	};
+	type: "album";
+	uri: string;
+	artists: Array<{
+		external_urls: {
+			spotify: string;
+		};
+		href: string;
+		id: string;
+		name: string;
+		type: "artist";
+		uri: string;
+	}>;
+	tracks: {
+		href: string;
+		limit: number;
+		next: string | null;
+		offset: number;
+		previous: string | null;
+		total: number;
+		items: Array<{
+			artists: Array<{
+				external_urls: {
+					spotify: string;
+				};
+				href: string;
+				id: string;
+				name: string;
+				type: "artist";
+				uri: string;
+			}>;
+			available_markets: string[];
+			disc_number: number;
+			duration_ms: number;
+			explicit: boolean;
+			external_urls: {
+				spotify: string;
+			};
+			href: string;
+			id: string;
+			is_playable?: boolean;
+			linked_from?: {
+				external_urls: {
+					spotify: string;
+				};
+				href: string;
+				id: string;
+				type: "track";
+				uri: string;
+			};
+			restrictions?: {
+				reason: string;
+			};
+			name: string;
+			preview_url: string | null;
+			track_number: number;
+			type: "track";
+			uri: string;
+			is_local: boolean;
+		}>;
+	};
+	copyrights: Array<{
+		text: string;
+		type: "C" | "P";
+	}>;
+	external_ids: {
+		isrc?: string;
+		ean?: string;
+		upc?: string;
+	};
+	genres: string[];
+	label: string;
+	popularity: number;
+};
