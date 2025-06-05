529a6df1e464301533c7c80cbf59008886d8fa66
monomonosu <pojinyan@yahoo.co.jp>
2025-06-05 23:52:09 +0900

    最新アルバム取得APIをoffset,limitで取得範囲選択できるように変更。APIRouteの追加。

--- src/app/_fetchers/getNewReleases.ts
+++ src/app/_fetchers/getNewReleases.ts
@@ -5,9 +5,13 @@ import { fetchSpotifyData } from "@/libs/spotify";
  * Spotify の新着アルバムを取得
  * @returns {Promise<SpotifyAlbumsResponse>}
  */
-export async function getNewReleases() {
+export async function getNewReleases({
+	offset = 0,
+	limit = 20,
+}: { offset?: number; limit?: number } = {}) {
 	const res = await fetchSpotifyData<SpotifyAlbumsResponse>(
-		"browse/new-releases",
+		`browse/new-releases?offset=${offset}&limit=${limit}&market=JP&language=ja-JP`,
 	);
+
 	return res;
 }
---
+++ src/app/api/spotify/new-release/route.ts
@@ -0,0 +1,31 @@
+import { getNewReleases } from "@/app/_fetchers/getNewReleases";
+import type { SpotifyAlbumsResponse } from "@/app/_fetchers/types";
+import { NextResponse } from "next/server";
+
+/**
+ * Spotifyの新着リリースを取得するAPIエンドポイント
+ */
+export async function GET(request: Request) {
+	const { searchParams } = new URL(request.url);
+	const offset = Number(searchParams.get("offset") || 0);
+	const limit = Number(searchParams.get("limit") || 20);
+
+	const res = await getNewReleases({
+		offset,
+		limit,
+	});
+	const data: SpotifyAlbumsResponse = await res;
+
+	try {
+		if (!data.albums || data.albums.items.length === 0) {
+			return NextResponse.json(
+				{ error: "No new releases found" },
+				{ status: 404 },
+			);
+		}
+
+		return NextResponse.json(data, { status: 200 });
+	} catch (error) {
+		return NextResponse.json({ error: error }, { status: 500 });
+	}
+}
