ef244ebf5ceae9807a5088d506b9dfca78f8e98b
monomonosu <pojinyan@yahoo.co.jp>
2025-06-09 03:50:46 +0900

    スケルトンにborder-radius追加

--- src/app/(static)/new/page.tsx
+++ src/app/(static)/new/page.tsx
@@ -41,7 +41,14 @@ export default function New() {
 				{isValidating && (
 					<InfiniteGrid>
 						{[...Array(24).keys()].map((i) => (
-							<Skeleton key={i} loading style={{ aspectRatio: "1 / 1" }}>
+							<Skeleton
+								key={i}
+								loading
+								style={{
+									borderRadius: "var(--radius-4)",
+									aspectRatio: "1 / 1",
+								}}
+							>
 								<div>
 									<br />
 								</div>
