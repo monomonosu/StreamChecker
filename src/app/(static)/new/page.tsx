5c24088797455cd6480973bab1d1d2ba146a3769
monomonosu <pojinyan@yahoo.co.jp>
2025-06-08 23:43:02 +0900

    newページでジャケットが等間隔で並ぶように修正

--- src/app/(static)/new/page.tsx
+++ src/app/(static)/new/page.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 import { useNewRelease } from "@/app/(static)/new/hook";
 import { Jacket } from "@/app/_components/server/Jacket/Jacket";
-import { Section } from "@/app/_styles/components/blocks";
-import { GapWrapper, PageWrapper } from "@/app/_styles/components/wrappers";
+import { InfiniteGrid, Section } from "@/app/_styles/components/blocks";
+import { PageWrapper } from "@/app/_styles/components/wrappers";
 
 export default function New() {
 	const { data, lastElementRef } = useNewRelease();
@@ -15,13 +15,14 @@ export default function New() {
 			<Section>
 				<h1>新着アルバム・シングル</h1>
 
-				<GapWrapper gap={24}>
+				<InfiniteGrid>
 					{data?.map((items) => (
 						<React.Fragment key={`parent-${items[0].id}`}>
 							{items.map((item) => (
 								<Jacket
 									key={item.id}
 									href={`/albums/${item.id}`}
+									fill
 									priority
 									src={item.images[0].url}
 									album={{ name: item.name, href: `/albums/${item.id}` }}
@@ -29,14 +30,12 @@ export default function New() {
 										name: item.artists[0].name,
 										href: `/artists/${item.artists[0].id}`,
 									}}
-									width={200}
-									height={200}
 									alt="最新リリースアルバム画像"
 								/>
 							))}
 						</React.Fragment>
 					))}
-				</GapWrapper>
+				</InfiniteGrid>
 
 				<div ref={lastElementRef}>最後の要素</div>
 			</Section>
