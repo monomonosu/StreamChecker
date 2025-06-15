562dd5c9815caae94cccde864044c7f1a1503fc8
monomonosu <pojinyan@yahoo.co.jp>
2025-06-15 23:57:20 +0900

    storybook9へのメジャーアップデート対応

--- src/stories/radix-uis/Button.stories.tsx
+++ src/stories/radix-uis/Button.stories.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@radix-ui/themes";
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/nextjs";
 
 const meta = {
 	title: "RadixUi/Button",
