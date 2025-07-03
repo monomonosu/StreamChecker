ee73384b15fe670ec0629fc2cb2f8c1e793445d0
monomonosu <pojinyan@yahoo.co.jp>
2025-07-04 03:33:32 +0900

    MV再生中にFooterPlayerに再生・停止状態を表示するようにした

--- src/app/_components/client/FooterPlayer/FooterPlayer.tsx
+++ src/app/_components/client/FooterPlayer/FooterPlayer.tsx
@@ -2,14 +2,21 @@
 
 import { Cross1Icon } from "@radix-ui/react-icons";
 import { Button, Theme } from "@radix-ui/themes";
+import Image from "next/image";
+import React from "react";
 
 import { useFooterPlayer } from "@/app/_components/client/FooterPlayer/useFooterPlayer";
 import { BasicText, LinkText } from "@/app/_styles/components/texts";
+import { GapWrapper } from "@/app/_styles/components/wrappers";
 
 import style from "@/app/_components/client/FooterPlayer/footer-player.module.scss";
 
-export const FooterPlayer = () => {
-	const { isOpenFooter, videoTitle, videoUrl, onClickClose } =
+type Props = {
+	theme: "dark" | "light";
+};
+
+export const FooterPlayer = ({ theme }: Props) => {
+	const { isOpenFooter, isPlaying, videoTitle, videoUrl, onClickClose } =
 		useFooterPlayer();
 
 	return (
@@ -19,7 +26,43 @@ export const FooterPlayer = () => {
 		>
 			<div className={style.footerMovie} id="youtube-player" />
 			<div className={style.footerContent}>
-				<BasicText>{videoTitle}</BasicText>
+				<GapWrapper gap={8}>
+					{theme === "dark" &&
+						(isPlaying ? (
+							<Image
+								width={24}
+								height={24}
+								src="/anime/wave_white.gif"
+								alt="再生中アニメーション白"
+							/>
+						) : (
+							<Image
+								width={24}
+								height={24}
+								src="/images/wave_white.png"
+								alt="停止中画像白"
+							/>
+						))}
+
+					{theme === "light" &&
+						(isPlaying ? (
+							<Image
+								width={24}
+								height={24}
+								src="/anime/wave_black.gif"
+								alt="再生中アニメーション黒"
+							/>
+						) : (
+							<Image
+								width={24}
+								height={24}
+								src="/images/wave_black.png"
+								alt="停止中画像黒"
+							/>
+						))}
+
+					<BasicText>{videoTitle}</BasicText>
+				</GapWrapper>
 				<LinkText href={videoUrl} target="_blank" rel="noopener noreferrer">
 					{videoUrl}
 				</LinkText>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
 					<GlobalSnackbar />
 					<div className={style.main}>
 						<div className={style["main-inner"]}>{children}</div>
-						<FooterPlayer />
+						<FooterPlayer theme={theme} />
 					</div>
 				</Theme>
 			</body>
