8fa7d5bcc4ceaafe35095073bb42b84b7eb26208
monomonosu <pojinyan@yahoo.co.jp>
2025-07-06 21:10:32 +0900

    テーマ切り替えトグルにhooks使用

--- src/app/_components/server/Header/client/ThemeToggle.tsx
+++ src/app/_components/server/Header/client/ThemeToggle.tsx
@@ -2,23 +2,14 @@
 
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { SegmentedControl } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+
+import { useTheme } from "@/utils/hooks/useTheme";
 
 export const ThemeToggle = () => {
-	const router = useRouter();
-	// cookieからテーマを取得
-	const cookieTheme = document.cookie
-		.split("; ")
-		.find((row) => row.startsWith("theme="));
-	const theme = cookieTheme ? cookieTheme.split("=")[1] : "dark"; // Default to 'dark' if not set
-
-	const handleChange = (value: string) => {
-		document.cookie = `theme=${value}; path=/; max-age=31536000`;
-		router.refresh();
-	};
+	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<SegmentedControl.Root defaultValue={theme} onValueChange={handleChange}>
+		<SegmentedControl.Root defaultValue={theme} onValueChange={toggleTheme}>
 			<SegmentedControl.Item value="dark">
 				<MoonIcon />
 			</SegmentedControl.Item>
