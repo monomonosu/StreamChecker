67813fa434664072506fc5aabce43d3297fd1f71
monomonosu <pojinyan@yahoo.co.jp>
2025-07-06 21:02:55 +0900

    ページ初期レンダリング時に現在適用しているテーマをグローバルステートにStoreするためのProvider作成

---
+++ src/app/_providers/ClientProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useTheme } from "@/utils/hooks/useTheme";
+
+export const ClientProvider = () => {
+	// ページ読み込み時にCookieからテーマをAtomにセット
+	const { setThemeValue } = useTheme();
+
+	const cookieTheme = document.cookie
+		.split("; ")
+		.find((row) => row.startsWith("theme="));
+	const theme = cookieTheme ? (cookieTheme.split("=")[1] as Theme) : "dark"; // Default to 'dark' if not set
+
+	setThemeValue(theme);
+
+	return null;
+};
