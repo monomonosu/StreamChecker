c7bd02088037fb5a6900b2961ceda0504a4d3534
monomonosu <pojinyan@yahoo.co.jp>
2025-07-08 01:27:20 +0900

    Trackをクリックした時の共通ロジックをhooksに切り出し

---
+++ src/utils/hooks/useTrack.ts
@@ -0,0 +1,33 @@
+import {
+	isOpenFooterAtom,
+	trackIdAtom,
+	trackQueueAtom,
+} from "@/libs/stores/video";
+import { useSetAtom } from "jotai";
+
+export const useTrack = () => {
+	const setTrackQueue = useSetAtom(trackQueueAtom);
+	const setTrackId = useSetAtom(trackIdAtom);
+	const setIsOpenFooter = useSetAtom(isOpenFooterAtom);
+
+	/**
+	 * @param {Track[]} tracks
+	 * @param {string} trackId
+	 * @description トラックをクリックした時に、トラックキューとトラックIDをセットし、フッターを開く
+	 */
+	const handleClickTrack = async ({
+		trackQueue,
+		trackId,
+	}: {
+		trackQueue: Track[];
+		trackId: string;
+	}) => {
+		setTrackQueue(trackQueue);
+		setTrackId(trackId);
+		setIsOpenFooter(true);
+	};
+
+	return {
+		handleClickTrack,
+	};
+};
