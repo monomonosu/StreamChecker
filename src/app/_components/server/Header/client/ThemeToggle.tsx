518d45b650004fd6f20e83cb132476fc9e971d33
monomonosu <pojinyan@yahoo.co.jp>
2025-07-21 22:22:33 +0900

    HeaderのMenuにAboutページへの導線追加

--- src/app/_components/server/Header/client/ThemeToggle.tsx
+++ src/app/_components/server/Header/client/ThemeToggle.tsx
@@ -3,13 +3,21 @@
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { SegmentedControl } from "@radix-ui/themes";
 
+import type { FC, HTMLAttributes } from "react";
+
 import { useTheme } from "@/utils/hooks/useTheme";
 
-export const ThemeToggle = () => {
+export const ThemeToggle: FC<HTMLAttributes<HTMLDivElement>> = ({
+	...props
+}) => {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<SegmentedControl.Root defaultValue={theme} onValueChange={toggleTheme}>
+		<SegmentedControl.Root
+			{...props}
+			defaultValue={theme}
+			onValueChange={toggleTheme}
+		>
 			<SegmentedControl.Item value="dark">
 				<MoonIcon />
 			</SegmentedControl.Item>
