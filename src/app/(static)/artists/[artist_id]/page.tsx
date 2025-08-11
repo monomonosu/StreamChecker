1039984f92b6e17bd570e488e5de7094c6c03626
monomonosu <pojinyan@yahoo.co.jp>
2025-08-12 03:07:16 +0900

    新規で作成したコンポーネントをアーティストページに適用

--- src/app/(static)/artists/[artist_id]/page.tsx
+++ src/app/(static)/artists/[artist_id]/page.tsx
@@ -1,18 +1,18 @@
-import { Jacket } from "@/app/_components/server/Jacket/Jacket";
+"use server";
 
-import { getAlbumsByArtist } from "@/app/_fetchers/getAlbumsByArtist";
-import { getArtist } from "@/app/_fetchers/getArtist";
-import { getTopTracksByArtist } from "@/app/_fetchers/getTopTracksByArtist";
+import { Suspense } from "react";
+
+import { Container as ArtistAlbumContainer } from "@/app/_components/server/Jacket/Artist/Container";
+import { Loading as ArtistAlbumLoading } from "@/app/_components/server/Jacket/Artist/Loading";
 
 import { Section, Slider } from "@/app/_styles/components/blocks";
 import { PageWrapper } from "@/app/_styles/components/wrappers";
 
-import { ArtistHeader } from "@/app/(static)/artists/[artist_id]/_components/ArtistHeader";
-import { TopTrackList } from "@/app/(static)/artists/[artist_id]/_components/TopTrackList";
+import { Container as ArtistHeaderContainer } from "@/app/(static)/artists/[artist_id]/_components/ArtistHeader/Container";
+import { Loading as ArtistHeaderLoading } from "@/app/(static)/artists/[artist_id]/_components/ArtistHeader/Loading";
 
-import style from "@/app/(static)/artists/[artist_id]/index.module.scss";
-
-import { PATH } from "@/utils/constants/path";
+import { Container as TopTracksContainer } from "@/app/(static)/artists/[artist_id]/_components/Track/TopTrackList/Container";
+import { Loading } from "@/app/(static)/artists/[artist_id]/_components/Track/TopTrackList/Loading";
 
 type Props = {
 	params: Promise<{ artist_id: string }>;
@@ -20,46 +20,28 @@ type Props = {
 
 export default async function ArtistPage({ params }: Props) {
 	const { artist_id } = await params;
-	const artist = await getArtist(artist_id);
-	const topTracks = await getTopTracksByArtist(artist_id);
-	const albums = await getAlbumsByArtist(artist_id);
 
 	return (
 		<PageWrapper>
 			<Section>
-				<ArtistHeader
-					src={
-						artist.images.length ? artist.images[0].url : "/images/no-image.png"
-					}
-					alt={artist.name}
-					artistName={artist.name}
-				/>
+				<Suspense fallback={<ArtistHeaderLoading />}>
+					<ArtistHeaderContainer artist_id={artist_id} />
+				</Suspense>
 			</Section>
 
 			<Section>
 				<h2>人気曲</h2>
-				<TopTrackList topTracks={topTracks} />
+				<Suspense fallback={<Loading />}>
+					<TopTracksContainer artist_id={artist_id} />
+				</Suspense>
 			</Section>
 
 			<Section>
 				<h2>アルバム</h2>
 				<Slider>
-					{albums.items.map((item) => (
-						<div className={style.jacketWrapper} key={item.id}>
-							<Jacket
-								href={PATH.ALBUMS(item.id)}
-								fill
-								priority
-								src={
-									item.images.length
-										? item.images[0].url
-										: "/images/no-image.png"
-								}
-								album={{ name: item.name, href: PATH.ALBUMS(item.id) }}
-								alt="アルバム画像"
-							/>
-						</div>
-					))}
+					<Suspense fallback={<ArtistAlbumLoading />}>
+						<ArtistAlbumContainer artist_id={artist_id} />
+					</Suspense>
 				</Slider>
 			</Section>
 		</PageWrapper>
