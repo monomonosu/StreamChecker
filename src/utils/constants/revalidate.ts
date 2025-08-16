fa8395dc84937b1ed930a6d3dd3e6332ec1ccd6e
monomonosu <pojinyan@yahoo.co.jp>
2025-08-16 21:28:22 +0900

    新着リリース・人気アルバム取得APIにrevalidateを設定

--- src/utils/constants/revalidate.ts
+++ src/utils/constants/revalidate.ts
@@ -1,6 +1,9 @@
 /**
  * revalidateの時間を定義する
+ * @constant {number} REVALIDATE_ONE_HOUR 1時間
  * @constant {number} REVALIDATE_ONE_MONTH 1ヶ月
  */
+
+export const REVALIDATE_ONE_HOUR = 3600; // 1時間
 
 export const REVALIDATE_ONE_MONTH = 2592000; // 1ヶ月
