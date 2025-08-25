4bd081376280021a09f9393c304585bfa73dd228
monomonosu <pojinyan@yahoo.co.jp>
2025-08-26 01:28:15 +0900

    アーティストのその他のアルバムの項目にSuspense・Containerを配置

--- src/app/(static)/albums/[album_id]/page.tsx
+++ src/app/(static)/albums/[album_id]/page.tsx
@@ -2,18 +2,13 @@ import { Suspense } from "react";
 
 import { Container as TrackListContainer } from "@/app/_components/client/Track/TrackList/Container";
 import { Loading as TrackListLoading } from "@/app/_components/client/Track/TrackList/Loading";
-
-import { Jacket } from "@/app/_components/server/Jacket/Jacket";
+import { Container as ArtistAlbumContainer } from "@/app/_components/server/Jacket/Artist/Container";
+import { Loading as ArtistAlbumLoading } from "@/app/_components/server/Jacket/Artist/Loading";
 import { getAlbum } from "@/app/_fetchers/getAlbum";
-import { getAlbumsByArtist } from "@/app/_fetchers/getAlbumsByArtist";
 import { Section, Slider } from "@/app/_styles/components/blocks";
 import { PageWrapper } from "@/app/_styles/components/wrappers";
-
 import { Container as AlbumHeaderContainer } from "@/app/(static)/albums/[album_id]/_components/AlbumHeader/Container";
 import { Loading as AlbumHeaderLoading } from "@/app/(static)/albums/[album_id]/_components/AlbumHeader/Loading";
-
-import style from "@/app/(static)/albums/[album_id]/index.module.scss";
-import { PATH } from "@/utils/constants/path";
 
 type Props = {
 	params: Promise<{ album_id: string }>;
@@ -22,7 +17,6 @@ type Props = {
 export default async function Album({ params }: Props) {
 	const { album_id } = await params;
 	const data = await getAlbum(album_id);
-	const artistAlbumsData = await getAlbumsByArtist(data.artists[0].id);
 
 	return (
 		<PageWrapper>
@@ -41,22 +35,9 @@ export default async function Album({ params }: Props) {
 			<Section>
 				<h2>アーティストのその他のアルバム</h2>
 				<Slider>
-					{artistAlbumsData.items.map((item) => (
-						<div className={style.jacketWrapper} key={item.id}>
-							<Jacket
-								href={PATH.ALBUMS(item.id)}
-								fill
-								priority
-								src={
-									item.images.length
-										? item.images[0].url
-										: "/images/no-image.png"
-								}
-								album={{ name: item.name, href: PATH.ALBUMS(item.id) }}
-								alt="アルバム画像"
-							/>
-						</div>
-					))}
+					<Suspense fallback={<ArtistAlbumLoading />}>
+						<ArtistAlbumContainer artist_id={data.artists[0].id} />
+					</Suspense>
 				</Slider>
 			</Section>
 		</PageWrapper>
