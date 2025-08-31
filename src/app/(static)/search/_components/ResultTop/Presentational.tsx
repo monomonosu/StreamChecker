3c344934e27e905bef743eec54a6349c97568f2b
monomonosu <pojinyan@yahoo.co.jp>
2025-09-01 01:39:23 +0900

    上位検索結果エリアのトラックコンポーネントリファクタ

--- src/app/(static)/search/_components/ResultTop/Presentational.tsx
+++ src/app/(static)/search/_components/ResultTop/Presentational.tsx
@@ -1,91 +1,84 @@
+import { Container } from "@/app/_components/client/Track/Container";
 import { Artist } from "@/app/_components/server/Artist/Artist";
 import { Jacket } from "@/app/_components/server/Jacket/Jacket";
-import type { SpotifySearchResponse } from "@/app/_fetchers/types";
+import type {
+	SpotifyAlbumItem,
+	SpotifyArtistItem,
+} from "@/app/_fetchers/types";
 import { Section } from "@/app/_styles/components/blocks";
 import { GapWrapper } from "@/app/_styles/components/wrappers";
-import { TrackArea } from "@/app/(static)/albums/[album_id]/_components/TrackArea";
 import style from "@/app/(static)/search/_components/ResultTop/index.module.scss";
 import { PATH } from "@/utils/constants/path";
-import { formatMsToMinSec } from "@/utils/helpers/formatDate";
 
 type Props = {
-	data: SpotifySearchResponse;
+	artist: SpotifyArtistItem;
+	album: SpotifyAlbumItem;
+	tracks: {
+		id: string;
+		title: string;
+		artist: string;
+		duration: string;
+	}[];
 };
 
 export const Presentational = (props: Props) => {
-	const { data } = props;
-	const { artists, albums, tracks } = data;
-
-	const artistItems = artists.items[0];
-	const albumItems = albums.items[0];
-	const trackItems = tracks.items.map((track) => ({
-		id: track.id,
-		title: track.name,
-		artist: track.artists[0].name,
-		duration: formatMsToMinSec(track.duration_ms),
-	}));
-
-	const isExistData = data || (artistItems && albumItems && trackItems);
+	const { artist, album, tracks } = props;
 
 	return (
-		<>
-			{isExistData && (
-				<Section>
-					<h2>上位の検索結果</h2>
+		<Section>
+			<h2>上位の検索結果</h2>
 
-					<GapWrapper gap={40} direction="row">
-						<GapWrapper gap={8} direction="column">
-							<h3>アーティスト</h3>
+			<GapWrapper gap={40} direction="row">
+				<GapWrapper gap={8} direction="column">
+					<h3>アーティスト</h3>
 
-							<div className={style.resultsWrapper}>
-								<Artist
-									href={PATH.ARTISTS(artistItems.id)}
-									fill
-									priority
-									src={
-										artistItems.images[0]
-											? artistItems.images[0].url
-											: "/images/no-image.png"
-									}
-									artist={{
-										name: artistItems.name,
-										href: PATH.ARTISTS(artistItems.id),
-									}}
-									alt="アーティスト画像"
-								/>
-							</div>
-						</GapWrapper>
+					<div className={style.resultsWrapper}>
+						<Artist
+							href={PATH.ARTISTS(artist.id)}
+							fill
+							priority
+							src={
+								artist.images[0] ? artist.images[0].url : "/images/no-image.png"
+							}
+							artist={{
+								name: artist.name,
+								href: PATH.ARTISTS(artist.id),
+							}}
+							alt="アーティスト画像"
+						/>
+					</div>
+				</GapWrapper>
 
-						<GapWrapper gap={8} direction="column">
-							<h3>アルバム</h3>
+				<GapWrapper gap={8} direction="column">
+					<h3>アルバム</h3>
 
-							<div className={style.resultsWrapper}>
-								<Jacket
-									href={PATH.ALBUMS(albumItems.id)}
-									fill
-									priority
-									src={
-										albumItems.images[0]
-											? albumItems.images[0].url
-											: "/images/no-image.png"
-									}
-									album={{
-										name: albumItems.name,
-										href: PATH.ALBUMS(albumItems.id),
-									}}
-									alt="アルバム画像"
-								/>
-							</div>
-						</GapWrapper>
+					<div className={style.resultsWrapper}>
+						<Jacket
+							href={PATH.ALBUMS(album.id)}
+							fill
+							priority
+							src={
+								album.images[0] ? album.images[0].url : "/images/no-image.png"
+							}
+							album={{
+								name: album.name,
+								href: PATH.ALBUMS(album.id),
+							}}
+							alt="アルバム画像"
+						/>
+					</div>
+				</GapWrapper>
 
-						<GapWrapper gap={8} direction="column">
-							<h3>トラック</h3>
+				<GapWrapper gap={8} direction="column">
+					<h3>トラック</h3>
 
-							<TrackArea tracks={trackItems.slice(0, 5)} />
-						</GapWrapper>
-					</GapWrapper>
-				</Section>
-			)}
-		</>
+					<div className={style.column}>
+						{tracks.map((track) => (
+							<Container key={track.id} tracks={tracks} track={track} />
+						))}
+					</div>
+				</GapWrapper>
+			</GapWrapper>
+		</Section>
 	);
 };
