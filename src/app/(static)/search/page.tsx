f188e5554a7dd37dd600bc76d12f14a461c5ad3b
monomonosu <pojinyan@yahoo.co.jp>
2025-09-02 02:34:28 +0900

    page修正

--- src/app/(static)/search/page.tsx
+++ src/app/(static)/search/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
-import { getSearchItems } from "@/app/_fetchers/getSearchItems";
 import { Section } from "@/app/_styles/components/blocks";
 import { PageWrapper } from "@/app/_styles/components/wrappers";
+import { Container as NonResultContainer } from "@/app/(static)/search/_components/NonResult/Container";
 import { Container as ResultAlbumsContainer } from "@/app/(static)/search/_components/ResultAlbum/Container";
 import { Loading as ResultAlbumsLoading } from "@/app/(static)/search/_components/ResultAlbum/Loading";
 import { Container as ResultArtistsContainer } from "@/app/(static)/search/_components/ResultArtist/Container";
@@ -18,34 +18,16 @@ type Props = {
 
 export default async function Search({ searchParams }: Props) {
 	const { query } = await searchParams;
-	if (!query) {
-		return (
-			<Section>
-				<h1>お気に入りのアーティスト・曲・アルバムを探しましょう！</h1>
-				<SearchForm />
-			</Section>
-		);
-	}
-
-	const data = query ? await getSearchItems(query) : null;
-	if (
-		!data ||
-		(data.artists.items.length === 0 &&
-			data.albums.items.length === 0 &&
-			data.tracks.items.length === 0)
-	) {
-		return (
-			<Section>
-				<h1>
-					お探しのものが見つからないようです。条件を変更して再度検索してください。
-				</h1>
-				<SearchForm />
-			</Section>
-		);
-	}
 
 	return (
 		<PageWrapper>
+			{!query && (
+				<Section>
+					<h1>お気に入りのアーティスト・曲・アルバムを探しましょう！</h1>
+					<SearchForm />
+				</Section>
+			)}
+
 			{query && (
 				<>
 					<Section>
@@ -67,6 +49,8 @@ export default async function Search({ searchParams }: Props) {
 					<Suspense fallback={<ResultTrackLoading />}>
 						<ResultTrackContainer query={query} />
 					</Suspense>
+
+					<NonResultContainer query={query} />
 				</>
 			)}
 		</PageWrapper>
