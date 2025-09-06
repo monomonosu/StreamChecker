2dad74621e2d5ab449202c2fea388e31b6f042da
monomonosu <pojinyan@yahoo.co.jp>
2025-09-06 21:59:42 +0900

    Loadingのスタイル変更

--- src/app/_components/server/Loading/loading.tsx
+++ src/app/_components/server/Loading/loading.tsx
@@ -2,19 +2,18 @@ import style from "./loading.module.scss";
 
 export default function Loading() {
 	return (
-		<div
-			className={style.loadingWrapper}
-			style={{ height: "calc(100vh - 56px - 48px * 2)" }}
-		>
-			<div>
-				<div className={style.loading}>
-					<div className={style.bar} />
-					<div className={style.bar} />
-					<div className={style.bar} />
-					<div className={style.bar} />
-					<div className={style.bar} />
+		<div className={style.loadingWrapper}>
+			<div className={style.loadingInner}>
+				<div>
+					<div className={style.loading}>
+						<div className={style.bar} />
+						<div className={style.bar} />
+						<div className={style.bar} />
+						<div className={style.bar} />
+						<div className={style.bar} />
+					</div>
+					<p className={style.loadingText}>Loading...</p>
 				</div>
-				<p className={style.loadingText}>Loading...</p>
 			</div>
 		</div>
 	);
