8f244fbdf31bc4b69fd62bca28122add21e79e91
monomonosu <pojinyan@yahoo.co.jp>
2025-09-09 04:34:55 +0900

    アーティストアルバムページの無限スクロールコンポーネント作成

---
+++ src/app/(static)/artists/[artist_id]/albums/_components/Albums/Presentational.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Jacket } from "@/app/_components/server/Jacket/Jacket";
+import type { SpotifyAlbumItem } from "@/app/_fetchers/types";
+import { InfiniteGrid } from "@/app/_styles/components/blocks";
+import { Loading } from "@/app/(static)/artists/[artist_id]/albums/_components/Albums/Loading";
+import { PATH } from "@/utils/constants/path";
+
+type Props = {
+	albums: (SpotifyAlbumItem[] | undefined)[] | undefined;
+	isValidation: boolean;
+	lastElementRef: React.RefObject<HTMLDivElement | null>;
+};
+
+export const Presentational = (props: Props) => {
+	const { albums, isValidation, lastElementRef } = props;
+
+	return (
+		<>
+			<InfiniteGrid>
+				{albums?.map((items) => (
+					<React.Fragment key={items ? `parent-${items[0].id}` : "no-items"}>
+						{items?.map((item) => (
+							<Jacket
+								key={item.id}
+								href={PATH.ALBUMS(item.id)}
+								fill
+								priority
+								src={item.images[0].url}
+								album={{ name: item.name, href: PATH.ALBUMS(item.id) }}
+								alt="最新リリースアルバム画像"
+							/>
+						))}
+					</React.Fragment>
+				))}
+
+				{isValidation && <Loading />}
+			</InfiniteGrid>
+
+			<div ref={lastElementRef} />
+		</>
+	);
+};
