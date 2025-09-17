0abb8645382c6d980c82948c249cad2e8e2e5283
monomonosu <pojinyan@yahoo.co.jp>
2025-09-17 23:27:53 +0900

    アーティストページで人気曲が１件も返ってこなかった場合の考慮追加

--- src/app/(static)/artists/[artist_id]/_components/Track/TopTrackList/Presentational.tsx
+++ src/app/(static)/artists/[artist_id]/_components/Track/TopTrackList/Presentational.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { BasicText } from "@/app/_styles/components/texts";
 import { Container } from "@/app/(static)/artists/[artist_id]/_components/Track/Container";
-
 import style from "./index.module.scss";
 
 type Props = {
@@ -13,16 +13,24 @@ export const Presentational = (props: Props) => {
 	const { tracks, trackGroups } = props;
 
 	return (
-		<div className={style.scrollWrapper}>
-			<div className={style.gridContainer}>
-				{trackGroups.map((group) => (
-					<div key={group[0].id} className={style.column}>
-						{group.map((track) => (
-							<Container key={track.id} tracks={tracks} track={track} />
+		<>
+			{tracks.length === 0 && (
+				<BasicText size={16}>曲が見つかりませんでした。</BasicText>
+			)}
+
+			{tracks.length > 0 && (
+				<div className={style.scrollWrapper}>
+					<div className={style.gridContainer}>
+						{trackGroups.map((group) => (
+							<div key={group[0].id} className={style.column}>
+								{group.map((track) => (
+									<Container key={track.id} tracks={tracks} track={track} />
+								))}
+							</div>
 						))}
 					</div>
-				))}
-			</div>
-		</div>
+				</div>
+			)}
+		</>
 	);
 };
