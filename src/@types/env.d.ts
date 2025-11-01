f0dcab49ff5fc9617db71e6483e69e775f04505a
monomonosu <pojinyan@yahoo.co.jp>
2025-11-01 23:54:50 +0900

    GA導入

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,7 +1,10 @@
 declare namespace NodeJS {
 	interface ProcessEnv {
+		readonly NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: string;
+
 		readonly YOUTUBE_API_KEY: string;
 		readonly YOUTUBE_API_BASE_URL: string;
+
 		readonly SPOTIFY_CLIENT_ID: string;
 		readonly SPOTIFY_CLIENT_SECRET: string;
 		readonly SPOTIFY_API_BASE_URL: string;
