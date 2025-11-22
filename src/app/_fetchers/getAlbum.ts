5803ec3a88f3ded9ae753cdfd95885fd993a5c97
monomonosu <pojinyan@yahoo.co.jp>
2025-11-23 00:16:11 +0900

    SporifyAPIのfetch時にrevalidateを適用してみる

--- src/app/_fetchers/getAlbum.ts
+++ src/app/_fetchers/getAlbum.ts
@@ -1,9 +1,11 @@
 import type { SpotifyAlbumResponse } from "@/app/_fetchers/types";
 import { fetchSpotifyData } from "@/libs/spotify";
+import { REVALIDATE_ONE_DAY } from "@/utils/constants/revalidate";
 
 export async function getAlbum(album_id: string) {
 	const res = await fetchSpotifyData<SpotifyAlbumResponse>(
 		`albums/${album_id}`,
+		REVALIDATE_ONE_DAY,
 	);
 
 	return res;
--- src/utils/constants/revalidate.ts
+++ src/utils/constants/revalidate.ts
@@ -1,9 +1,12 @@
 /**
  * revalidateの時間を定義する
  * @constant {number} REVALIDATE_ONE_HOUR 1時間
+ * @constant {number} REVALIDATE_ONE_DAY 1日
  * @constant {number} REVALIDATE_ONE_MONTH 1ヶ月
  */
 
 export const REVALIDATE_ONE_HOUR = 3600; // 1時間
+
+export const REVALIDATE_ONE_DAY = 86400; // 1日
 
 export const REVALIDATE_ONE_MONTH = 2592000; // 1ヶ月
