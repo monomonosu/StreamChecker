cbc2ad11244bf220d604f15e32936bcb4ac18e1e
monomonosu <pojinyan@yahoo.co.jp>
2025-12-20 23:34:24 +0900

    マウスでのスクロールにSliderを対応

--- src/app/_styles/components/blocks/index.tsx
+++ src/app/_styles/components/blocks/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useRef } from "react";
 import styled from "@/app/_styles/components/blocks/index.module.scss";
 
 interface SectionProps {
@@ -15,8 +18,33 @@ interface SliderProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export const Slider = ({ children, className, style }: SliderProps) => {
+	const sliderRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const slider = sliderRef.current;
+		if (!slider) return;
+
+		const handleWheel = (e: WheelEvent) => {
+			// 縦スクロールの場合、横スクロールに変換
+			if (e.deltaY !== 0) {
+				e.preventDefault();
+				slider.scrollLeft += e.deltaY;
+			}
+		};
+
+		slider.addEventListener("wheel", handleWheel, { passive: false });
+
+		return () => {
+			slider.removeEventListener("wheel", handleWheel);
+		};
+	}, []);
+
 	return (
-		<div className={`${className} ${styled.slider}`} style={style}>
+		<div
+			ref={sliderRef}
+			className={`${className} ${styled.slider}`}
+			style={style}
+		>
 			{children}
 		</div>
 	);
